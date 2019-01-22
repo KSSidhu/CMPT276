29bf64cb0a211765f6d9e0259a1c0215d752f62f
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2019-01-22 12:31:39 -0800

    Add HTML file, Update chip8.js
    Redesigned the chip8 object and added reset function. Now runs initial tests

--- chip8.js
+++ chip8.js
@@ -1,373 +1,303 @@
 // Referenced http://blog.alexanderdickson.com/javascript-chip-8-emulator for chip 8 processor construction
 // Reference http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/ for reset function construction
 
-// Chip 8 processor
-var chip8 = function(){
-	this.initialize();
 
-	wihle(true){
-		this.runCycle();
-	}
-};
+chip8 = {
+	// Program Counter
+	pc: 0,
 
+	//Memory
+	memory: new Uint8Array(4096), // Standard Chip8 memory size, 4096 bytes
 
+	//Stack
+	stack: new Array(16), // According to Wikipedia page, modern systems use 16 levels
 
-chip8.prototype = {
+	//Stack Pointer
+	sp: 0,
 
-	var hexFontset =[
-	  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-	  0x20, 0x60, 0x20, 0x20, 0x70, // 1
-	  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-	  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-	  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-	  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-	  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-	  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-	  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-	  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-	  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-	  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-	  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-	  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-	  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-	  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-	];
+	//"V" Registers
+	v: new Array(16), // V[0] -> V[F]
 
+	// Video Memory
+	vram: new Array(64 * 32),
 
-	initialize: function() {
+	//Keyboard Buffer
+	buffer: new Array(16),
 
-		//Program Counter
-		this.pc = 0
+	// Draw Operation Flag
+	drawFlag: false,
 
-		//Memory
-		this.memory = new Array(0x1000); // Standard Chip8 memory size, 4096 bytes
+	//Key Press
+	keyPress: false,
 
-		//Stack
-		this.stack = new Array(16); // According to Wikipedia page, modern systems use 16 levels
+	// "I" Index Registers
+	i: 0,
 
-		//Stack Pointer
-		this.sp = 0;
+	// Delay Timer
+	delayTimer: 0,
 
-		//"V" Registers
-		this.v = new Array(16); // V[0] -> V[F]
-
-		// Video Memory
-		this.vram = new Array(64 * 32);
-
-		//Keyboard Buffer
-		this.buffer = new Array(16);
-
-		// Draw Operation Flag
-		this.drawFlag = false;
-
-		//Key Press
-		this.keyPress = false;
-
-		// "I" Index Registers
-		this.i = 0;
-
-		// Delay Timer
-		this.delayTimer = 0;
-
-		//Sound Timer
-		this.soundTimer = 0;
-	};
+	//Sound Timer
+	soundTimer: 0,
 
 	// Resets parameters of the emulator, saved into a reset() function
 
-	reset: function() { // Used to initialize chip8 emulator
+	reset: function() {
+		// Used to initialize chip8 emulator
 
-		var k;
-
-		for(k = 0; k < this.memory.length; k++){ // Load fontset
-			this.memory[k] = hexFontset[k]
-		};
-
-		for(k = 0; k < this.v.length; k++){
-			this.v[k] = 0;
-		};
+		chip8.memory = chip8.memory.map( ()=> 0);
+		chip8.v = chip8.v.map( ()=> 0);
 
 		// reset stack pointers
-		this.sp = 0;
-		this.i = 0;
+		chip8.sp = 0;
+		chip8.i = 0;
 
 		// reset the PC to 0x200
-		this.pc = 0x200;
+		chip8.pc = 0x200;
 
 		// Reset timers
-		this.delayTimer = 0;
-		this.soundTimer = 0;
-	};
+		chip8.delayTimer = 0;
+		chip8.soundTimer = 0;
+	},
 
 	//Emulation Cycle
-	runCycle: function(){
+	runCycle: function() {
 		//Fetch Opcode
-		var opcode = memory[pc] << 8 | memory[pc + 1]; // obtain 16-bit opcode command
-													//Grab top 8 bits of opcode, shift left by 8 to make space to add remaining 8 bits of opcode
+		var opcode = (chip8.memory[chip8.pc] << 8) | chip8.memory[chip8.pc + 1]; // obtain 16-bit opcode command
+		//Grab top 8 bits of opcode, shift left by 8 to make space to add remaining 8 bits of opcode
+
+		
+		
 
 		//Decode Opcode
 
-		switch(opcode & 0xF000) { // Check 4 most significant bits
-
+		switch (opcode & 0xf000) { // Check 4 most significant bits
 			case 0x0000:
-				switch(opcode & 0x000F) { // Check least 4 significant bits
-
+				switch (opcode & 0x000f) { // Check least 4 significant bits
 					case 0x0000: //0x000E Clears display
+						break;
 
-					break;
-
-					case 0x000E:
-						sp --;
+					case 0x000e:
+						sp--;
 						pc = stack[sp]; // push PC to top of stack
 						pc += 2;
-					break;
+						break;
 				}
-			break;
+				break;
 
-			var x = (opcode & 0x0F00) >> 8; 
-			var y = (opcode & 0x00F0) >> 4; 
+				var x = (opcode & 0x0f00) >> 8;
+				var y = (opcode & 0x00f0) >> 4;
 
 			case 0x1000:
-				pc = opcode & 0x0FFF;
-			break;
-
+				pc = opcode & 0x0fff;
+				break;
 
 			case 0x2000:
 				stack[sp] = pc;
 				sp++;
-				pc = opcode & 0x0FFF;
-			break;
-
+				pc = opcode & 0x0fff;
+				break;
 
 			case 0x3000:
-				if(v[x] == (opcode & 0x00FF)) { //compare V[x] to last 8 bits
+				if (v[x] == (opcode & 0x00ff)) {
+					//compare V[x] to last 8 bits
 					pc += 4;
 				} else {
 					pc += 2;
 				}
-			break;
-
+				break;
 
 			case 0x4000:
-				if(v[x] != (opcode & 0x00FF)) { //compare V[x] to last 8 bits
+				if (v[x] != (opcode & 0x00ff)) {
+					//compare V[x] to last 8 bits
 					pc += 4;
 				} else {
 					pc += 2;
 				}
-			break;
-
+				break;
 
 			case 0x5000:
-				if(v[x] == v[y]) {
+				if (v[x] == v[y]) {
 					pc += 4;
 				} else {
 					pc += 2;
 				}
-			break;
-
+				break;
 
 			case 0x6000:
-				v[x] = (opcode & 0x00FF);
+				v[x] = opcode & 0x00ff;
 				pc += 2;
-			break;
-
+				break;
 
 			case 0x7000:
-				v[x] += (opcode & 0x00FF);
+				v[x] += opcode & 0x00ff;
 				pc += 2;
-			break;
-
+				break;
 
 			case 0x8000:
-				switch(opcode & 0x000F) {
+				switch (opcode & 0x000f) {
 					case 0x0000:
 						v[x] = v[y];
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0001:
-						x = opcode & 0x0F00;
-						v[x] = (v[x] | v[y]);
+						x = opcode & 0x0f00;
+						v[x] = v[x] | v[y];
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0002:
-						v[x] = (v[x] & v[y]);
+						v[x] = v[x] & v[y];
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0003:
-						v[x] = (v[x] ^ v[y]);
+						v[x] = v[x] ^ v[y];
 						pc += 2;
-					break;
+						break;
 
-
-					case 0x0004;
-						if(v[y] > (0xFF - v[x]))
-						    v[0xF] = 1; //carry
-						 else
-						    v[0xF] = 0;
-						 v[x] += v[y];
-  						pc += 2; 
-					break;
-
+					case 0x0004:
+						if (v[y] > 0xff - v[x])
+							v[0xf] = 1; //carry
+						else v[0xf] = 0;
+						v[x] += v[y];
+						pc += 2;
+						break;
 
 					case 0x0005:
-						if(v[x] > v[y]) { // Vx > Vy
-							v[0xF] = 1; //carry
+						if (v[x] > v[y]) {
+							// Vx > Vy
+							v[0xf] = 1; //carry
 						} else {
-							v[0xF] = 0
+							v[0xf] = 0;
 						}
 
 						v[x] -= v[y]; //Vx = Vx - Vy
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0006:
-						if(v[(opcode & 0x0100) >> 8] == 1) {
-							v[0xF] = 1;
+						if (v[(opcode & 0x0100) >> 8] == 1) {
+							v[0xf] = 1;
 						} else {
-							v[0xF] = 0;
+							v[0xf] = 0;
 						}
 
 						v[x] = v[x] >> 1;
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0007:
-						if(v[y] > v[x]) { // Vy > Vx
-							v[0xF] = 1;
+						if (v[y] > v[x]) {
+							// Vy > Vx
+							v[0xf] = 1;
 						} else {
-							v[0xF] = 0;
+							v[0xf] = 0;
 						}
 
 						v[x] = v[y] - v[x]; // Vx = Vy - Vx
 						pc += 2;
-					break;
+						break;
 
-
-					case 0x000E:
-						if(v[(opcode & 0x0900) >> 11]) {
-							v[0xF] = 1;
+					case 0x000e:
+						if (v[(opcode & 0x0900) >> 11]) {
+							v[0xf] = 1;
 						} else {
-							v[0xF] = 0;
+							v[0xf] = 0;
 						}
 
 						v[x] << 1;
 						pc += 2;
-					break;
+						break;
 				}
 
-
-			case 0x9000;
-				if(v[x] != v[y]) { // Vx != Vy ?
+			case 0x9000:
+				if (v[x] != v[y]) {
+					// Vx != Vy ?
 					pc += 4;
 				} else {
 					pc += 2;
 				}
-			break;
+				break;
 
-
-			case 0xA000: // ANNN : Sets I to address NNN
-				i = opcode & 0x0FFF; // This case grabs the last 12 bits to analyze
+			case 0xa000: // ANNN : Sets I to address NNN
+				i = opcode & 0x0fff; // This case grabs the last 12 bits to analyze
 				pc += 2;
-			break;
+				break;
 
+			case 0xb000:
+				pc = (opcode & 0x0fff) + v[0];
+				break;
 
-			case 0xB000:
-				pc = (opcode & 0x0FFF) + v[0];
-			break;
-
-
-			case 0xC000:
-				v[x] = (Math.random() * 256) & (opcode & 0x00FF);
+			case 0xc000:
+				v[x] = (Math.random() * 256) & (opcode & 0x00ff);
 				pc += 2;
-			break;
+				break;
 
-
-			case 0xD000:
-				var height = opcode & 0x000F // save nibble
+			case 0xd000:
+				var height = opcode & 0x000f; // save nibble
 				var sprite;
 
-				v[0xF] = 0;
+				v[0xf] = 0;
 
-				for(int ylim = 0; ylim < height; y++){
+				for (var ylim = 0; ylim < height; y++) {
 					sprite = v[i + ylim];
 
-					for(var xlim = 0; xlim < 8; xlim++){
-						if((sprite & (0x80 >> xlim)) != 0){
-							if(vram[(v[x] + xlim + ((v[y] + ylim) * 64))] == 1) {
-								v[0xF] = 1;
+					for (var xlim = 0; xlim < 8; xlim++) {
+						if ((sprite & (0x80 >> xlim)) != 0) {
+							if (vram[v[x] + xlim + (v[y] + ylim) * 64] == 1) {
+								v[0xf] = 1;
 							}
-							vram[(v[x] + xlim + ((v[y] + ylim) * 64))] ^= 1
+							vram[v[x] + xlim + (v[y] + ylim) * 64] ^= 1;
 						}
 					}
 				}
 
-			break;
+				break;
 
+			case 0xe000:
+				break;
 
-			case 0xE000:
-			break;
+			case 0xf000:
+				switch (opcode & 0x00ff) {
+					case 0x0007:
+						v[x] = delayTimer;
+						pc += 2;
+						break;
 
-
-			case 0xF000:
-				switch(opcode & 0x00FF) {
-					case 0x0007
-					v[x] = delayTimer;
-					pc += 2;
-					break;
-
-
-					case 0x000A
-
-					break;
-
+					case 0x000a:
+						break;
 
 					case 0x0015:
-						delayTimer = v[x]
+						delayTimer = v[x];
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0018:
-						soundTimer = v[x]
+						soundTimer = v[x];
 						pc += 2;
-					break;
+						break;
 
-
-					case 0x001E:
-						i += v[x]
+					case 0x001e:
+						i += v[x];
 						pc += 2;
-					break;
-
+						break;
 
 					case 0x0029:
-					break;
-
+						break;
 
 					case 0x0033:
-					break;
-
+						break;
 
 					case 0x0055:
-					break;
-
+						break;
 
 					case 0x0065:
-					break;
+						break;
 				}
-			
 
 			default:
-				console.log("Unknown Opcode [0x0000]: opcode");
-
+				console.log('Unknown Opcode [0x0000]: opcode');
 		}
-	};
-
+	}
 };
