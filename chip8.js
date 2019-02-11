c2d733c1022ed76315548bf67ee29b0eeb8d2793
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2019-02-11 09:59:54 -0800

    update chip8.js and visualizer.html

--- chip8.js
+++ chip8.js
@@ -1,3 +1,5 @@
+
+
 // Reference http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/ for chip8 object layout and functions to include
 // Referenced http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#00E0 for opcode instructions
 
@@ -50,7 +52,7 @@ chip8 = {
 	drawFlag: false,
 
 	//Key Press
-	keyPressed: false,
+	keyPress: false,
 
 	// "I" Index Register
 	i: 0,
@@ -131,16 +133,18 @@ chip8 = {
 		chip8.soundTimer = 0;
 
 		// Grab the canvas element to draw on
-		// chip8.canvas = document.getElementById('romDisplay').getContext('2d');
+		chip8.canvas = document.getElementById('romDisplay').getContext('2d');
 
 		//reset flags
 		drawFlag = false;
 		loadFlag = false;
-		keyPressed = false;
+		keyPress = false;
 		keyWait = false;
 
 
 	},
+
+	
 
 	loadGame: function(file) {
 		let reader = new FileReader();
@@ -356,18 +360,15 @@ chip8 = {
 
 				chip8.v[0xf] = 0;
 
-				let v_X = chip8.v[x];
-				let v_Y = chip8.v[y];
-
 				for (var ylim = 0; ylim < height; y++) {
-					sprite = chip8.v[chip8.i + ylim];
+					sprite = chip8.v[i + ylim];
 
 					for (var xlim = 0; xlim < 8; xlim++) {
 						if ((sprite & (0x80 >> xlim)) != 0) {
-							if (chip8.vram[v_X + xlim + (v_Y + ylim) * 64] == 1) { // checks if any sprites currently exist at position
+							if (chip8.vram[v[x] + xlim + (chip8.v[y] + ylim) * 64] == 1) { // checks if any sprites currently exist at position
 								chip8.v[0xf] = 1;
 							}
-							chip8.vram[v_X + xlim + (v_Y + ylim) * 64] ^= 1; // draw sprite to screen
+							chip8.vram[v[x] + xlim + (chip8.v[y] + ylim) * 64] ^= 1; // draw sprite to screen
 						}
 					}
 				}
@@ -455,7 +456,7 @@ chip8 = {
 					//Store registers v0 through vX in memory at i
 					case 0x0055:
 						for(let i = 0; i <= x; i++) {
-							chip8.memory[chip8.i + i] = chip8.v[i];
+							chip8.v[i] = chip8.memory[chip8.i + i];
 						}
 						break;
 
@@ -473,38 +474,11 @@ chip8 = {
 		}
 	},
 
-	keyPress: function()
-	{
-		alert("Hello");
-	},
+/******************************************
+Keyboard Handling
 
 
-	render: function() {
-		// If there's nothing to draw, return
-		if(chip8.drawFlag === false) {
-			return;
-		}
-
-		chip8.canvas.fillStyle = "#aaa";
-		chip8.canvas.fillRect(0, 0, 640, 320);
-		chip8.canvas.fillStyle = "#FF9100";
-
-		for(let i = 0; i < chip8.vram.length; i++) {
-			if(chip8.vram[i] == 1) {
-				let y = i / 64 | 0;
-				let x = i - 64*y;
-				chip8.canvas.fillRect(x*15,y*15,15,15);
-			}
-		}
-
-		chip8.drawFlag = false;
-	},
-
-	/******************************************
-	Keyboard Handling
-
-
-	******************************************/
+******************************************/
 	keyPress: function(index)
 	{
 		 translateKeys = {
@@ -538,8 +512,33 @@ chip8 = {
 		chip8.keyLog[keyCode] = keyCode;
 	}, //
 
+
+/******************************************
+Remder/Draw
+
+
+******************************************/
+	render: function() {
+		// If there's nothing to draw, return
+		if(chip8.drawFlag === false) {
+			return;
+		}
+
+		chip8.canvas.fillStyle = "#aaa";
+		chip8.canvas.fillRect(0, 0, 640, 320);
+		chip8.canvas.fillStyle = "#FF9100";
+
+		for(let i = 0; i < chip8.vram.length; i++) {
+			if(chip8.vram[i] == 1) {
+				let y = i / 64 | 0;
+				let x = i - 64*y;
+				chip8.canvas.fillRect(x*15,y*15,15,15);
+			}
+		}
+
+		chip8.drawFlag = false;
+	} //
 };
-
-
+	
 
 module.exports = chip8; // exporting the chip8 object to run tests with JEST.js
